ec0f0f85e7b00165669f948f5ea9ac5840b7a9c7
marmooo <marmooo@users.noreply.github.com>
2021-05-05 08:53:29 +0900

    update libraries

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2021-03-01 11:30';
+var CACHE_NAME = '2021-05-04 08:50';
 var urlsToCache = [
   '/touch-abc/',
   '/touch-abc/index.js',
@@ -10,7 +10,12 @@ var urlsToCache = [
   '/touch-abc/mp3/correct3.mp3',
   '/touch-abc/mp3/incorrect1.mp3',
   '/touch-abc/mp3/stupid5.mp3',
-  'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@15.0.0/dist/smooth-scroll.polyfills.min.js',
+  'https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js',
+  'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"',
+  'https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js',
+  'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js',
 ];
 
 self.addEventListener('install', function(event) {
