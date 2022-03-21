170e45ebefa156470288f059c4d2d0696f8359a5
marmooo <marmooo@users.noreply.github.com>
2022-03-21 09:25:18 +0900

    bump signature_pad from 4.0.2 to 4.0.3

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2022-03-21 01:05';
+var CACHE_NAME = '2022-03-21 09:11';
 var urlsToCache = [
   "/touch-abc/",
   "/touch-abc/index.js",
@@ -11,7 +11,7 @@ var urlsToCache = [
   "/touch-abc/mp3/incorrect1.mp3",
   "/touch-abc/mp3/stupid5.mp3",
   "/touch-abc/favicon/original.svg",
-  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.2/dist/signature_pad.umd.min.js",
+  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.3/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
   "https://fonts.googleapis.com/css2?family=Aref+Ruqaa&family=Sansita+Swashed&family=Neucha&family=Bree+Serif&family=Amaranth&family=ABeeZee&family=Sriracha&family=Farsan&family=Ranga&family=Delius+Swash+Caps&family=Kotta+One&family=Bellota&family=Chilanka&family=Poor+Story&family=Paprika&display=swap",
