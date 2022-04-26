1396e38098dba5540d6fbd4d8d7dcfac0c45c4c5
marmooo <marmooo@users.noreply.github.com>
2022-04-27 08:32:01 +0900

    add OGP

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '2022-04-05 00:14';
+var CACHE_NAME = '2022-04-27 08:30';
 var urlsToCache = [
   "/touch-abc/",
   "/touch-abc/index.js",
@@ -10,10 +10,10 @@ var urlsToCache = [
   "/touch-abc/mp3/correct3.mp3",
   "/touch-abc/mp3/incorrect1.mp3",
   "/touch-abc/mp3/stupid5.mp3",
-  "/touch-abc/favicon/original.svg",
-  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.4/dist/signature_pad.umd.min.js",
+  "/touch-abc/favicon/favicon.svg",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
+  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.4/dist/signature_pad.umd.min.js",
   "https://fonts.googleapis.com/css2?family=Aref+Ruqaa&family=Sansita+Swashed&family=Neucha&family=Bree+Serif&family=Amaranth&family=ABeeZee&family=Sriracha&family=Farsan&family=Ranga&family=Delius+Swash+Caps&family=Kotta+One&family=Bellota&family=Chilanka&family=Poor+Story&family=Paprika&display=swap",
 ];
 
