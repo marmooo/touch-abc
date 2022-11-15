4f605628086d34ac0c49bbfbe20a356fa2c854a6
marmooo <marmooo@users.noreply.github.com>
2022-11-16 00:09:17 +0900

    disable double tap zoom

--- src/drill.js
+++ src/drill.js
@@ -653,3 +653,9 @@ document.addEventListener("click", unlockAudio, {
   once: true,
   useCapture: true,
 });
+
+// disable troublesome iOS features
+// - double tap zoom
+document.ondblclick = (e) => {
+  e.preventDefault();
+};
