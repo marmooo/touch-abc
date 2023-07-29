8d6e35bcfd4fb5c434e1cdd478692d5182ef9533
marmooo <marmooo@users.noreply.github.com>
2023-07-29 10:21:50 +0900

    bump bootstrap from 5.3.0 to 5.3.1

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2023-07-22 11:11";
+var CACHE_NAME = "2023-07-29 10:17";
 var urlsToCache = [
   "/touch-abc/",
   "/touch-abc/index.yomi",
@@ -12,7 +12,7 @@ var urlsToCache = [
   "/touch-abc/mp3/stupid5.mp3",
   "/touch-abc/favicon/favicon.svg",
   "https://marmooo.github.io/yomico/yomico.min.js",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.1.6/dist/signature_pad.umd.min.js",
   "https://fonts.googleapis.com/css2?family=Aref+Ruqaa&family=Sansita+Swashed&family=Neucha&family=Bree+Serif&family=Amaranth&family=ABeeZee&family=Sriracha&family=Farsan&family=Ranga&family=Delius+Swash+Caps&family=Kotta+One&family=Bellota&family=Chilanka&family=Poor+Story&family=Paprika&display=swap",
 ];
