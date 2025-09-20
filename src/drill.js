c30f4965df1fcc69c5f05eb8101efbb3c332d08c
marmooo <marmooo@users.noreply.github.com>
2025-09-20 11:19:04 +0900

    bump signature_pad from 5.1.0 to 5.1.1

--- src/drill.js
+++ src/drill.js
@@ -1,4 +1,4 @@
-import signaturePad from "https://cdn.jsdelivr.net/npm/signature_pad@5.1.0/+esm";
+import signaturePad from "https://cdn.jsdelivr.net/npm/signature_pad@5.1.1/+esm";
 
 let audioContext;
 const audioBufferCache = {};
