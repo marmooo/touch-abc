c7f3395d793c0d6e2cc6e8698250bf910db6939f
marmooo <marmooo@users.noreply.github.com>
2025-12-06 10:17:42 +0900

    bump signature_pad from 5.1.2 to 5.1.3

--- src/drill.js
+++ src/drill.js
@@ -1,4 +1,4 @@
-import signaturePad from "https://cdn.jsdelivr.net/npm/signature_pad@5.1.2/+esm";
+import signaturePad from "https://cdn.jsdelivr.net/npm/signature_pad@5.1.3/+esm";
 import { createWorker } from "https://cdn.jsdelivr.net/npm/emoji-particle@0.0.4/+esm";
 
 let audioContext;
